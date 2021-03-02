64e9fceadd50e1a51cc6e76bc4198af68ddcf980
ajith11031999 <59559841+ajith11031999@users.noreply.github.com>
2021-03-02 19:00:11 +0530

    Add files via upload

---
+++ 02-03-2021/js18.pl
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript typeof</h2>
+<p>The typeof operator returns the type of a variable or an expression.</p>
+
+<p id="typeof"></p>
+
+<script>
+document.getElementById("typeof").innerHTML = 
+typeof "" + "<br>" +
+typeof "Ajith" + "<br>" + 
+typeof 21 + "<br>" +
+typeof function myFunc(){};
+</script>
+</body>
+</html>

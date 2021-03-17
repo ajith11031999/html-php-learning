b7d1d91988504222a91e30790d1f1a5f0bac2876
ajith11031999 <59559841+ajith11031999@users.noreply.github.com>
2021-03-17 18:03:48 +0530

    Update file.php

--- Training/file.php
+++ Training/file.php
@@ -1,39 +1,24 @@
-PHP file upload
-
 <!DOCTYPE html>
+<?php
+
+$cookie_name = "user";
+$cookie_value = "rain";
+setcookie($cookie_name, $cookie_value, time() + (20), "/");
+?>
 <html>
 <body>
-<!-- html form for selecting file -->
-<form action="file6.php" method="post" enctype="multipart/form-data">  
-  Select File:  
-    <input type="file" name="fileToUpload"/>  
-    <input type="submit" value="Upload" name="submit"/>  
-</form>
+
+<?php
+if(!isset($_COOKIE[$cookie_name])) {
+     echo "Cookie named '" . $cookie_name . "' is not set!";
+} else {
+     echo "Cookie '" . $cookie_name . "' is set!<br>";
+    
+     echo "Value is: " . $_COOKIE[$cookie_name];
+}
+?>
+
+<p>Reload the page to see the value of the cookie.</p>
 
 </body>
 </html>
-
-
-<?php  
-
-$target_path = "/home/ajith/Documents/CSS";  
-$target_path = $target_path.basename( $_FILES['fileToUpload']['name']);   
-  
-if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_path)) {  
-    echo "File uploaded successfully!";  
-} else{  
-    echo "Sorry, file not uploaded, please try again!";  
-  }  
-
-?>  
-
-
-
-
-
-
-
-
-
-
-

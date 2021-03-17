69cc798da6f1bc067306e6456f919f64ef1355c3
ajith11031999 <59559841+ajith11031999@users.noreply.github.com>
2021-03-17 16:40:45 +0530

    Update file.php

--- Training/file.php
+++ Training/file.php
@@ -1,9 +1,39 @@
-<?php
-//fwrite
-$myfile = fopen("open.txt", "w") or die("Unable to open file!");
-$txt = "Hello World!\n";
-fwrite($myfile, $txt);
-$txt = "Good Day!\n";
-fwrite($myfile, $txt);
-fclose($myfile);
-?>
+PHP file upload
+
+<!DOCTYPE html>
+<html>
+<body>
+<!-- html form for selecting file -->
+<form action="file6.php" method="post" enctype="multipart/form-data">  
+  Select File:  
+    <input type="file" name="fileToUpload"/>  
+    <input type="submit" value="Upload" name="submit"/>  
+</form>
+
+</body>
+</html>
+
+
+<?php  
+
+$target_path = "/home/ajith/Documents/CSS";  
+$target_path = $target_path.basename( $_FILES['fileToUpload']['name']);   
+  
+if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_path)) {  
+    echo "File uploaded successfully!";  
+} else{  
+    echo "Sorry, file not uploaded, please try again!";  
+  }  
+
+?>  
+
+
+
+
+
+
+
+
+
+
+

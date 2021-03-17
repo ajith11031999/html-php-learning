c55cea0284946121c7a57d5f77d6d3607a6ef979
ajith11031999 <59559841+ajith11031999@users.noreply.github.com>
2021-03-17 14:31:50 +0530

    Update file.php

--- Training/file.php
+++ Training/file.php
@@ -1,10 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
 $myfile = fopen("meeting.txt", "r") or die("Unable to open file!");
+// Output one character until end-of-file
 
-// Output one line until end-of-file
+echo fgetc($myfile);
 
-while(!feof($myfile)) {
-  echo fgets($myfile) . "<br>";
-}
 fclose($myfile);
 ?>
+
+</body>
+</html>

beb99781c6df5130b0eb8d31be8e5570fba1fa49
ajith11031999 <59559841+ajith11031999@users.noreply.github.com>
2021-03-25 14:30:26 +0530

    Update prototype.php

--- Training/prototype.php
+++ Training/prototype.php
@@ -1,35 +1,71 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE HTML>
+<html> 
+<style>
+input[type=text], select {
+  width: 100%;
+  padding: 10px ;
+  margin: 8px ;
+  display: inline-block;
+  border: 1px red;
+  border-radius: 40px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 25%;
+  background-color: red;
+  color: white;
+  padding: 10px;
+  margin:auto;
+  display:block;
+  border: 1px red;
+  border-radius: 40px;
+  cursor: pointer;
+  
+}
+
+input[type=submit]:hover {
+  background-color: blue;
+}
+
+div {
+  border-radius: 50px;
+  width: 50%;
+  margin:auto;
+  display:block;
+  background-color: black;
+  padding: 20px;
+}
+</style>
+<script>
+let valid = {
+  validateForm() {
+  var x = document.forms["myForm"]["firstname"].value;
+  if (x == "") {
+    alert("First name should be filled");
+  }
+}
+};
+let inherit = {
+__proto__: valid
+};
+
+</script> 
 <body>
 
-<h2>JavaScript prototype inheritance</h2>
+<div>
 
-<p id="demo"></p>
-
-<script>
-let animal = {
-  eats: true,
-  walk() {
-    alert("Animal walk");
-  }
-};
-
-let rabbit = {
-  jumps: true,
-  __proto__: animal
-};
-
-let longEar = {
-  earLength: 10,
-  __proto__: rabbit
-};
-
-
-longEar.walk(); 
-alert(longEar.jumps); 
-document.getElementById("demo").innerHTML = longEar.jumps;
-</script>
-
+<form name="myForm" action="class.php"  onsubmit="inherit.validateForm();" method="post">
+<label for="firstname" style=" color: white">First Name</label>
+<input type="text" name="firstname" placeholder="Enter your first name"><br><br>
+<label for="lastname" style=" color: white">Last Name</label>
+<input type="text" name="lastname" placeholder="Enter your last name"><br><br> 
+<label for="email" style=" color: white">E-mail</label>
+<input type="text" name="email" placeholder="Enter your email"><br><br>
+<label for="phno" style=" color: white">Phno</label>
+<input type="text" name="phno" placeholder="Enter your phno"><br><br>
+<input type="submit"  name ="submit" value="Submit" >
+</form>
+</div>
 </body>
 </html>
-

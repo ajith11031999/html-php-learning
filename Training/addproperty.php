96a9c8ee052299f5d455f6ca9e2c581c7043b873
ajith11031999 <59559841+ajith11031999@users.noreply.github.com>
2021-03-25 18:37:46 +0530

    Update addproperty.php

--- Training/addproperty.php
+++ Training/addproperty.php
@@ -5,7 +5,7 @@
 <h2>JavaScript Objects</h2>
 
 <p id="demo"></p>
-
+<p id="demo2"></p>
 <script>
 function Person(first, last, age, eye) {
   this.firstName = first;
@@ -17,7 +17,13 @@ function Person(first, last, age, eye) {
 Person.prototype.nation = "India";
 
 var object1 = new Person("Ajith", "xavier", 22, "brown");
-document.getElementById("demo").innerHTML = "My name is " + object1.firstName +". "+ "I am from  " + object1.nation; 
+document.getElementById("demo").innerHTML = "My name is " + object1.firstName +". "+ "I am from  " + object1.nation;
+
+var object2 = new Person("Ravi", "kumar", 50 , "brown");
+document.getElementById("demo2").innerHTML = "My name is " + object2.firstName +". "+ "I am from  " + object2.nation;
+
+
+
 </script>
 
 </body>

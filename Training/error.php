e399881fefb8f4d86f9e47cd98fdc2bedb994be6
ajith11031999 <59559841+ajith11031999@users.noreply.github.com>
2021-03-26 10:11:31 +0530

    Update error.php

--- Training/error.php
+++ Training/error.php
@@ -1,25 +1,33 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
+
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 error_reporting(-1);
 
-try {
-   $x = "123456789";
-   $y = "12345";
-   echo $z;
-   include ("nothing.php");
-   add($x, $y);
-   echo $x;
-}
-catch (exception $e) {
-      echo $e->getMessage();
-}
+class DatabaseConfig{
+    const host  = "localhost";  
+    private $user="ajith";
+    private $pass="Aspire@123";
+    private $db="php";
+    
+ public function Connect()
+ { 
+  try{
+       if ($conn=mysqli_connect(self::host, $this->user, $this->pass, $this->db)){
+        echo"connected";
+        return $conn;  
+       }
+       else{
+         throw new Exception('Unable to connect');
+       }
+     }
+  catch(Exception $e){
+    echo $e->getMessage();
+     }
 
-?> 
+ }
+}
+  
+$dbConn = new DatabaseConfig();
+$conn =$dbConn->Connect();
 
-</body>
-</html>

bac93d5a785e7b85f73c5d60d01ff000b21362bf
ajith11031999 <59559841+ajith11031999@users.noreply.github.com>
2021-03-29 18:22:10 +0530

    Update oops.php

--- Training/oops.php
+++ Training/oops.php
@@ -1,26 +1,35 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
 class Info{
   public $name;
   public $age;
   public static $var = "Hello";
-
+  const CONST = "Good Day!";
   function __construct($name, $age) {
     $this->name = $name; 
     $this->age = $age;
   }
-  
-      
-    public static function func() {
-        echo self::$var . ",";
-    }
-  
+     
   function __destruct() {
     echo "My name is {$this->name} and I am {$this->age} years old."; 
   }
+  
+   
+  public function cnst() {
+    echo self::CONST;
+  }
+  
+  public static function func() {
+        echo self::$var . ",";
+    }
 }
 
-$apple = new Info("Ajith",'22');
+$obj = new Info("Ajith",'22');
 Info::func();
+$obj->cnst();
 ?>
  
 </body>

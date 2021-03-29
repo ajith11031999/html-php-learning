45519325d06a9e5803af941f2fb8fb66fade3c33
ajith11031999 <59559841+ajith11031999@users.noreply.github.com>
2021-03-29 18:07:45 +0530

    Update oops.php

--- Training/oops.php
+++ Training/oops.php
@@ -1,22 +1,26 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
 class Info{
   public $name;
   public $age;
+  public static $var = "Hello";
 
   function __construct($name, $age) {
     $this->name = $name; 
     $this->age = $age;
   }
+  
+      
+    public static function func() {
+        echo self::$var . ",";
+    }
+  
   function __destruct() {
     echo "My name is {$this->name} and I am {$this->age} years old."; 
   }
 }
 
 $apple = new Info("Ajith",'22');
+Info::func();
 ?>
  
 </body>

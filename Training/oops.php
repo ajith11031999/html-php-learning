b003d369f5f4bbd9d9975ecc79ba3b67000dd31b
ajith11031999 <59559841+ajith11031999@users.noreply.github.com>
2021-03-29 22:37:54 +0530

    Update oops.php

--- Training/oops.php
+++ Training/oops.php
@@ -10,7 +10,7 @@ ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 error_reporting(-1);
 
-include "abstract.php";
+include "config.php";
 $id = $_GET['id'];
 
  interface Update{

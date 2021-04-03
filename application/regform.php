e435687e9396abe78e28733ce8b91b1886fbc5fe
ajith11031999 <59559841+ajith11031999@users.noreply.github.com>
2021-04-03 19:01:25 +0530

    Update regform.php

--- application/regform.php
+++ application/regform.php
@@ -8,39 +8,31 @@
 <body>  
 
 <?php
-
-$tdate = date('Y-m-d'); 
+$usernameErr = $passwordErr = $emailErr = $phnoErr = "";
+$username = $password = $email = $phno = ""; 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["username"]) || preg_match("/^[a-zA-Z ]+$/", $_POST["username"] == false)) {
-    $usernameErr = "Enter a valid username username";
+  if (empty($_POST["username"]) || !preg_match("/^[a-zA-Z-' ]*$/", $_POST["username"] )) {
+    $usernameErr = "Only letters and white space allowed";
   } else {
     $username = test_input($_POST["username"]);
   }
-    if (empty($_POST["password"]) || strlen($_POST["password"])<=8 || preg_match("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$", $_POST["password"] == false)) {
+    if (empty($_POST["password"]) || strlen($_POST["password"])<=8 || !preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/", $_POST["password"])) {
     $passwordErr = "Entered password is not strong";
   } else {
     $password = test_input($_POST["password"]);
   }
   
-  if (empty($_POST["email"])||preg_match("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", $_POST["email"] == false)) {
+  if (empty($_POST["email"])|| !preg_match("/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/", $_POST["email"])) {
     $emailErr = "please enter valid email id";
   } else {
     $email = test_input($_POST["email"]);
   }
-   if (empty($_POST["phno"]) ||preg_match("^[6-9][0-9]{9}$", $_POST["phno"] == false)) {
+   if (empty($_POST["phno"]) || !preg_match("/^[6-9][0-9]{9}$/", $_POST["phno"])) {
     $phnoErr = "please enter a valid phone number";
   } else {
     $phno = test_input($_POST["phno"]);
-  }
-    if (empty($_POST["date"]) || strcmp($_POST["date"],$tdate) != 0 ) {
-    
-    $dateErr = "please enter today's date";
-    
-  } else {
-    $date = test_input($_POST["date"]);
-  }
-    
+  }   
 }
 
 function test_input($data) {
@@ -55,20 +47,17 @@ function test_input($data) {
 <h2>Enter the following details</h2>
 <p><span class="error">* required field</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  Username: <input type="text" name="username"  pattern="^[a-zA-Z ]+$" required>
+  Username: <input type="text" name="username" value="<?php echo $_POST["username"] ?>">
   <span class="error">* <?php echo $usernameErr;?></span>
   <br><br>
-  Password: <input type="text" name="password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$" required >
+  Password: <input type="text" name="password" value="<?php echo $_POST["password"] ?>">
   <span class="error">* <?php echo $passwordErr;?></span>
   <br><br>
-  E-mail: <input type="text" name="email" pattern="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$" required >
+  E-mail: <input type="text" name="email" value="<?php echo $_POST["email"] ?>">
   <span class="error">* <?php echo $emailErr;?></span>
   <br><br>
-  Phno: <input type="text" name="phno" pattern="^[6-9][0-9]{9}$" required>
+  Phno: <input type="text" name="phno" value="<?php echo $_POST["phno"] ?>">
   <span class="error">* <?php echo $phnoErr;?></span>
-  <br><br>
-  Date: <input type="text" name="date" placeholder="YYYY-MM-DD"  required>
-  <span class="error">* <?php echo $dateErr;?></span>
   <br><br>
   <input type="submit" name="submit" value="Submit">  
 </form>
@@ -76,6 +65,21 @@ function test_input($data) {
 <?php  
 include "config.php";
 
+if(isset($_POST["submit"])){ 
+    if(!empty($usernameErr or $passwordErr or $emailErr or $phnoErr)){
+     echo "Not registered!!";
+     return false;
+    }
+    $records = mysqli_query($conn,"select phno from users");
+    while($data = mysqli_fetch_array($records)){     
+         if($data['phno'] == $phno){
+          echo "Not registered!!<br>";
+          echo "phone number already exist"; 
+          return false;
+         }
+       }  
+   
+}
 
 
 echo $username;
@@ -86,33 +90,20 @@ echo $email;
 echo "<br>";
 echo $phno;
 echo "<br>";
-echo $date;
-echo "<br>";
 
-if($date = ''){
-header("Location: http://localhost/regform.php");
-}
 class Register{
- function Insert($conn,$username,$password,$email,$phno,$date){  
-   if($username == "" or $password =="" or $email == "" or $phno =="" or $date ==""){
-        
-   }    
-      $sql = "INSERT INTO users (username,password,email,phno,created_date) VALUES ('$username', '$password', '$email', '$phno','$date')";   
-        
-
-        if(mysqli_query($conn, $sql)){
+  function Insert($conn,$username,$password,$email,$phno){ 
+    $sql = "INSERT INTO users (username,password,email,phno,created_date) VALUES ('$username', '$password', '$email', '$phno')";   
+    if(mysqli_query($conn, $sql)){
          echo "Inserted into the table<br>";  
-         header("Location: http://localhost/login.php");  
-        }
     }
-
+  }
 }
-
-
-
-
+$insert = new Register();
+$insert->Insert($conn,$username,$password,$email,$phno);
     
 ?>
 
 </body>
 </html>
+

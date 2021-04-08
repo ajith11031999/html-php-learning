ccf19e9dbf094c63bc864fb92bb02d193ee6ef55
ajith11031999 <59559841+ajith11031999@users.noreply.github.com>
2021-04-08 23:25:50 +0530

    Update admin.php

--- application/admin.php
+++ application/admin.php
@@ -2,34 +2,46 @@
 <html>
 <head>
   <title>Admin page </title>
+  <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" href="http://cdn.datatables.net/plug-ins/3cfcc339e89/integration/bootstrap/3/dataTables.bootstrap.css">
+
 </head>
+
 <body>
-<h3>Welcome admin</h3>
-<h4>Incase of any change click the edit option</h4>
-      
-<table border="2">
-  <tr>
-    <td>Id</td>
-    <td>User Name</td>
-    <td>Password</td>
-    <td>Phno</td>
-    <td>Date</td>
-    <td>Edit</td>
-  </tr>
+<div class="container">
+	<div class="row">
+		<h2 class="text-center">Users details</h2>
+	</div>
+    
+        <div class="row">
+		
+            <div class="col-md-12">
+
+                <table id="datatable" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                    <thead>	
+                          <tr>
+                               <th>ID</th>		
+                               <th>Username</th>
+                               <th>Password</th>
+                               <th>Phno</th>
+                               <th>Date</th>
+                               <th>Edit</th>
+                               <th>Delete</th>
+                          </tr>
+                    </thead>
+
+
 <?php
 
-
 include "config.php";
-
-
-
 
 class Store  {
      
     public function edit($conn){     
       $records = mysqli_query($conn,"select * from users"); // fetch data from database
       while($data = mysqli_fetch_array($records)){
-      ?>
+      ?> <tbody>
          <tr>
          <td><?php echo $data['id']; ?></td>
          <td><?php echo $data['username']; ?></td>
@@ -37,7 +49,10 @@ class Store  {
          <td><?php echo $data['phno']; ?></td> 
          <td><?php echo $data['modified_date']; ?></td>
          <td><a href="adminform.php?id=<?php echo $data['id']; ?>">Edit</a></td>   
+          <td><a href="adminform.php?id=<?php echo $data['id']; ?>">Delete</a></td>   
          </tr>	
+	</tbody>
+						
 
       <?php
        }
@@ -49,9 +64,12 @@ $obj = new Store();
 $obj->edit($conn);
 
 
-?> 
-
-</table>
+?> 					
+                </table>
+            </div>
+        </div>
+</div>
 
 </body>
-</html>
+    
+<html>    

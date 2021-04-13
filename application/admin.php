20ada74f8253fc4126b714363287d303e300e66b
ajith11031999 <59559841+ajith11031999@users.noreply.github.com>
2021-04-13 21:06:22 +0530

    Update admin.php

--- application/admin.php
+++ application/admin.php
@@ -9,11 +9,49 @@
 
 <link rel="stylesheet" type="text/css" href="http://cdn.datatables.net/plug-ins/3cfcc339e89/integration/bootstrap/3/dataTables.bootstrap.css">
 
+
+
 </head>
 
 <body>
+<script>
 
+function demoDisplay() {
+  document.getElementById("container2").style.display = "block";
+}
+</script>
+<div id="block_container">
 <div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            
+            <div id="sidebar" class="well sidebar-nav">
+              
+                <ul class="nav nav-pills nav-stacked">
+                    <li class="active"><a href="#">Home</a></li>
+                </ul>
+                <h5><i class="glyphicon glyphicon-user"></i>
+                    <small><b>USERS</b></small>
+                </h5>
+                <ul class="nav nav-pills nav-stacked">
+                    <li><a href="#">List</a></li>
+                    <li><a onclick="demoDisplay()" id="user-manage">Manage users</a></li>
+                </ul>
+                <h5><i class="glyphicon glyphicon-user"></i>
+                    <small><b>Role</b></small>
+                </h5>
+                <ul class="nav nav-pills nav-stacked">
+                    <li><a href="#">List</a></li>
+                    <li><a href="role.php">Manage role</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <!-- Content Here -->
+        </div>
+    </div>
+</div>
+<div id="container2" style="display:none">
 	<div class="row">
 		<h2 class="text-center">Users details</h2>
 		
@@ -75,6 +113,7 @@ $obj->edit($conn);
           <span class="glyphicon glyphicon-log-out"></span> Log out
         </a>
       </p> 
+</div>
 </div>
 
 </body>

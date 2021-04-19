099101226e428829bdc6e2cc9d962d2eafebc481
ajith11031999 <59559841+ajith11031999@users.noreply.github.com>
2021-04-19 21:29:02 +0530

    Update session.php

--- application/session.php
+++ application/session.php
@@ -10,7 +10,24 @@
    
    $login_session = $row['username'];
    
-   if(!isset($_SESSION['username'])){
+    $pages = array();
+       $records = mysqli_query($conn,"SELECT users.role_id FROM users WHERE  username = '$user_check' ");
+       $data = mysqli_fetch_array($records);
+       $role_id = $data['role_id'];
+       
+       $records = mysqli_query($conn,"SELECT page_name FROM permissions WHERE  role_id = '$role_id' "); 
+       while($data = mysqli_fetch_array($records)){
+       $pages[] = $data['page_name'];
+       }
+      
+       $link = $_SERVER['REQUEST_URI'];
+       $url = str_replace("/","","$link");
+       
+       if( !in_array( "$url" ,$pages ) ) {
+            header("location:login.php");
+        } 
+       
+   if(!isset($_SESSION['username']) ){
       header("location:login.php");
       die();
    }

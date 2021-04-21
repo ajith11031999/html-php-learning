569b876f4e17efc20e4bbd39ae12190dbd07bc3a
ajith11031999 <59559841+ajith11031999@users.noreply.github.com>
2021-04-21 21:30:46 +0530

    Update permissions.php

--- application/permissions.php
+++ application/permissions.php
@@ -1,16 +1,80 @@
+<?php
+   include('session.php');
+?>
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Display all records from Database</title>
-</head>
+  <title>Admin page </title>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 
+<link rel="stylesheet" href="bootcss.css">
+
+</head>
 <body>
-<form method = "post">
-<table border="2">
-        <tr>
+<script>
+
+function demoDisplay() {
+  document.getElementById("container2").style.display = "block";
+}
+</script>
+<div class="container-fluid">
+   <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container-fluid">
+	<!-- Brand and toggle get grouped for better mobile display -->
+	   <div class="navbar-header">
+	        <div class="navbar-brand">
+                    <h4>Welcome <?php echo $login_session; ?></h4> 
+                </div>	
+	   </div>
+					
+       <ul class="nav navbar-nav navbar-right">
+       <li><a href="logout.php" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+       </ul>
+       </div><!-- /.navbar-collapse -->
+       </div><!-- /.container-fluid -->
+    </nav>  	
+   <div class="container-fluid main-container">
+ 	<div class="col-md-2 sidebar">
+	    <div class="row">
+	     <!-- uncomment code for absolute positioning tweek see top comment in css -->
+		<div class="absolute-wrapper"> </div>  
+		  <!-- Menu -->
+		   <div class="side-menu">
+		      <nav class="navbar navbar-default" role="navigation">
+			 <!-- Main Menu -->
+		           <div class="side-menu-container">
+		               <ul class="nav navbar-nav">
+		               <li class="active"><a href="admin.php"><span class="glyphicon glyphicon-dashboard"></span>Home</a></li>
+				<li><a href="admin.php"><span class="glyphicon glyphicon-plane"></span> Manage User</a></li>
+				<li><a href="role.php"><span class="glyphicon glyphicon-plane"></span> Manage role</a></li>
+				<li><a onclick="demoDisplay()" id="user_manage"><span class="glyphicon glyphicon-cloud"></span> Manage permissions</a></li>
+				</ul>
+	      		   </div><!-- /.navbar-collapse -->
+		      </nav>
+		   </div>
+              </div>  		
+	 </div>
+        <div class="col-md-8 content">
+	   <div class="panel panel-default">
+	      <div class="panel-body">
+<div id="container2" style="display:none">
+		<div class="row">
+		<h2 class="text-center">Page details</h2>
+		
+	</div>
+    
+        <div class="row">
+		
+            <div class="col-md-12">
+                 <form>
+                 <table id="datatable" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                       <tr>
          <th>Pages</th>
+                   
 <?php
-include "config.php";
+
 $admin = $editor = $user = '';
       $records = mysqli_query($conn,"select role_name from role"); // fetch data from database
       while($data = mysqli_fetch_array($records)){
@@ -50,6 +114,7 @@ $admin = $editor = $user = '';
 
 
 ?> 
+
 </table>
 <input type="submit" name="submit" Value="Submit"/>
 <form>
@@ -89,5 +154,13 @@ if(isset($_POST['submit'])){
 
 }
 ?>
-</body>
-</html>
+                </table>
+                
+            </div>
+        </div>
+</div>
+	      </div>
+	   </div>
+	</div>
+</body>	
+</html>	
